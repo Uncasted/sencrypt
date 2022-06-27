8d7609e5f5eba80eaad5d0e3dde1a537fcb96e28
Juan Ferreira <juanferf30@gmail.com>
2022-06-27 17:30:22 -0500

    Refactor AddAcount for much better readability.

--- src/view/password-manager/src/components/Accounts/AddAccount.jsx
+++ src/view/password-manager/src/components/Accounts/AddAccount.jsx
@@ -2,7 +2,7 @@ export function AddAccount() {
     return (
         <label htmlFor="my-modal"
                className="ml-4 modal-button bg-black px-6 py-3 text-white hover:bg-black-1 active:bg-black
-               transition hover:cursor-pointer">
+               transition hover:cursor-pointer shadow-lg">
             Add new account
         </label>
     );
@@ -14,33 +14,65 @@ export function AccountModal() {
             <input type="checkbox" id="my-modal" className="modal-toggle"/>
             <div className="modal">
                 <div className="modal-box bg-white rounded-none px-0 py-0 w-[400px]">
-                    <div className="bg-black text-white w-full py-4 pl-4 mb-4">
-                        <label htmlFor="my-modal"
-                               className="btn bg-transparent border-none absolute right-2 top-1 text-white rounded-none
-                           hover:bg-black">✕</label>
-                        <h1 className="text-xl">Add new Account:</h1>
-                    </div>
+                    <ModalHeader/>
                     <form className="flex flex-col items-center space-y-4">
-                        <label htmlFor="website">
-                            <p className="text-lg">Website:</p>
-                            <input type="text" id="website" name="website"
-                                   className="input bg-white border-black rounded-none"/>
-                        </label><label htmlFor="username">
-                        <p className="text-lg">Username:</p>
-                        <input type="text" id="username" name="username"
-                               className="input bg-white border-black rounded-none" required/>
-                    </label><label htmlFor="password">
-                        <p className="text-lg">Password:</p>
-                        <input type="password" id="password" name="password"
-                               className="input bg-white border-black rounded-none" required/>
-                    </label>
+                        <Website/>
+                        <Username/>
+                        <Password/>
                         <div className="modal-action">
-                            <label htmlFor="my-modal" className="bg-black px-6 py-3 text-white hover:bg-black-1
-                            active:bg-black transition hover:cursor-pointer mb-4 mt-4">Add Account</label>
+                            <SubmitAccount/>
                         </div>
                     </form>
                 </div>
             </div>
         </>
     )
+}
+
+function ModalHeader() {
+    return (
+        <div className="bg-black text-white w-full py-4 pl-4 mb-4">
+            <label htmlFor="my-modal"
+                   className="btn bg-transparent border-none absolute right-2 top-1 text-white rounded-none
+                           hover:bg-black">✕</label>
+            <h1 className="text-xl">Add new Account:</h1>
+        </div>
+    );
+}
+
+function Website() {
+    return (
+        <label htmlFor="website">
+            <p className="text-lg">Website:</p>
+            <input type="text" id="website" name="website"
+                   className="input bg-white border-gray-500 rounded-none h-10"/>
+        </label>
+    )
+}
+
+function Username() {
+    return (
+        <label htmlFor="username">
+            <p className="text-lg">Username:</p>
+            <input type="text" id="username" name="username"
+                   className="input bg-white border-gray-500 rounded-none h-10" required/>
+        </label>
+    );
+}
+
+function Password() {
+    return (
+        <label htmlFor="password">
+            <p className="text-lg">Password:</p>
+            <input type="password" id="password" name="password"
+                   className="input bg-white border-gray-500 rounded-none h-10" required/>
+        </label>
+    );
+}
+
+function SubmitAccount() {
+    return (
+        <label htmlFor="my-modal" className="bg-black px-6 py-3 text-white hover:bg-black-1
+            active:bg-black transition hover:cursor-pointer mb-4 mt-4">Add Account</label>
+    );
 }

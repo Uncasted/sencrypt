5627411a24da950b4c27be7aa46039b267c61802
Uncasted <juanferf30@gmail.com>
2022-07-01 17:53:07 -0500

    Create context for add/remove account.

---
+++ src/view/password-manager/src/components/Accounts/Context/AccountsContext.jsx
@@ -0,0 +1,52 @@
+import {createContext, useContext, useState} from "react"
+
+const AccountsContext = createContext()
+const AccountsContextUpdate = createContext()
+
+export function useAccountsContext() {
+    return useContext(AccountsContext)
+}
+
+export function useAccountsContextUpdate() {
+    return useContext(AccountsContextUpdate)
+}
+
+export default function AccountsProvider(props) {
+    // Placeholder information
+    const myAccounts = [{
+        website: "steamcommunity.com",
+        username: "username",
+        password: "password"
+    }, {
+        website: "google.com",
+        username: "username@gmail.com",
+        password: "admin123"
+    }]
+
+    const [accounts, setAccounts] = useState(myAccounts)
+
+    // I have to make a copy otherwise It doesn't work.
+    const createAccount = (data) => {
+        setAccounts(accounts => {
+            const newAccounts = [...accounts]
+            newAccounts.push(data)
+            return newAccounts
+        })
+    }
+
+    const removeAccount = (index) => {
+        setAccounts(accounts => {
+            const newAccounts = [...accounts]
+            newAccounts.splice(index, 1)
+            return newAccounts
+        })
+    }
+
+    return (
+        <AccountsContext.Provider value={accounts}>
+            <AccountsContextUpdate.Provider value={{createAccount, removeAccount}}>
+                {props.children}
+            </AccountsContextUpdate.Provider>
+        </AccountsContext.Provider>
+    )
+}

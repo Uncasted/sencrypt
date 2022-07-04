520f4a0473ecb25bd6ecf71f7b1fe6e45931e722
Uncasted <juanferf30@gmail.com>
2022-07-03 22:48:20 -0500

    Add import statement to import the images. Add Login screen.

--- src/view/password-manager/src/App.jsx
+++ src/view/password-manager/src/App.jsx
@@ -1,16 +1,37 @@
 import {Navbar} from "./components/Navbar"
 import {Accounts} from "./components/Accounts/Accounts"
 import AccountsProvider from "./components/Accounts/Context/AccountsContext"
+import LoginScreen from "./components/LoginScreen"
+
+import logo from '../images/logo.png'
+import managerIcon from '../images/password-icon.png'
+import generatorIcon from '../images/generator-icon.png'
+import settingsIcon from '../images/settings-icon.png'
+import clipboardIcon from '../images/clipboard-icon.png'
+import showPasswordIcon from '../images/show-password-icon.png'
+import hidePasswordIcon from '../images/hide-password-icon.png'
+
+export const images = {
+    logo: logo,
+    managerIcon: managerIcon,
+    generatorIcon: generatorIcon,
+    settingsIcon: settingsIcon,
+    clipboardIcon: clipboardIcon,
+    showPasswordIcon: showPasswordIcon,
+    hidePasswordIcon: hidePasswordIcon
+}
 
 export default function App() {
     return (
         <>
-            <Navbar/>
-            <div className="ml-[250px]">
-                <AccountsProvider>
-                    <Accounts/>
-                </AccountsProvider>
-            </div>
+            <LoginScreen>
+                <Navbar/>
+                <div className="ml-[250px]">
+                    <AccountsProvider>
+                        <Accounts/>
+                    </AccountsProvider>
+                </div>
+            </LoginScreen>
         </>
     )
 }

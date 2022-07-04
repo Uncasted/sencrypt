d29f8b9965b7f0096d1aaecde9f1eeb0b677239c
Uncasted <juanferf30@gmail.com>
2022-07-04 17:27:15 -0500

    Create Placeholder menus for other sections of the navbar.

--- src/view/password-manager/src/App.jsx
+++ src/view/password-manager/src/App.jsx
@@ -2,6 +2,7 @@ import {Navbar} from "./components/Navbar"
 import {Accounts} from "./components/Accounts/Accounts"
 import AccountsProvider from "./components/Accounts/Context/AccountsContext"
 import LoginScreen from "./components/LoginScreen"
+import {useState} from "react"
 
 import logo from '../images/logo.png'
 import managerIcon from '../images/password-icon.png'
@@ -24,16 +25,47 @@ export const images = {
 
 
 export default function App() {
+    const [selected, setSelected] = useState(<AccountSection/>)
+
+    const changeSelected = (menu) => {
+        switch (menu) {
+            case 'Accounts':
+                setSelected(<AccountSection/>)
+                break
+            case 'Generator':
+                setSelected(<MenuPlaceholder/>)
+                break
+            case 'Settings':
+                setSelected(<MenuPlaceholder/>)
+        }
+    }
+
     return (
         <>
             <LoginScreen>
-                <Navbar/>
-                <div className="ml-[250px]">
-                    <AccountsProvider>
-                        <Accounts/>
-                    </AccountsProvider>
-                </div>
+                <Navbar changeSelected={changeSelected}/>
+                {selected}
             </LoginScreen>
         </>
+    )
+}
+
+function AccountSection() {
+    return (
+        <div className="ml-[250px]">
+            <AccountsProvider>
+                <Accounts/>
+            </AccountsProvider>
+        </div>
+    )
+}
+
+// Placeholder for other menus in the navbar, will change.
+function MenuPlaceholder() {
+    return (
+        <div className="ml-[250px] h-[85vh] flex flex-col items-center justify-center">
+            <h1 className="text-2xl text-gray-300">This section is not available right now.</h1>
+            <h1 className="text-lg text-gray-300">It will get implemented in a future update.</h1>
+        </div>
     )
 }

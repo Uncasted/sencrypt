ff4b6b483e9a67f59f0c4558336d7bd3bbbc68e9
Juan Ferreira <juanferf30@gmail.com>
2022-07-10 12:41:23 -0500

    Add progress slider tracker for input range.

--- src/view/password-manager/src/App.jsx
+++ src/view/password-manager/src/App.jsx
@@ -1,8 +1,9 @@
 import {Navbar} from "./components/Navbar"
 import {Accounts} from "./components/Accounts/Accounts"
 import AccountsProvider from "./components/Accounts/Context/AccountsContext"
+import {Generator} from "./components/Generator/Generator"
 import LoginScreen from "./components/LoginScreen"
-import {useState} from "react"
+import {useEffect, useState} from "react"
 
 import logo from '/assets/images/logo.png'
 import managerIcon from '/assets/images/manager-icon.png'
@@ -27,13 +28,24 @@ export const images = {
 export default function App() {
     const [selected, setSelected] = useState(<AccountSection/>)
 
+    useEffect(() => {
+        // Progress bar for the password generator length.
+        // Thank you https://github.com/toughengineer!
+        for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
+            e.style.setProperty('--value', e.value)
+            e.style.setProperty('--min', e.min == '' ? '0' : e.min)
+            e.style.setProperty('--max', e.max == '' ? '100' : e.max)
+            e.addEventListener('input', () => e.style.setProperty('--value', e.value))
+        }
+    })
+
     const changeSelected = (menu) => {
         switch (menu) {
             case 'Accounts':
                 setSelected(<AccountSection/>)
                 break
             case 'Generator':
-                setSelected(<MenuPlaceholder/>)
+                setSelected(<Generator/>)
                 break
             case 'Settings':
                 setSelected(<MenuPlaceholder/>)

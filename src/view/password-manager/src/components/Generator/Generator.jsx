bbf03cb139955ac5299e95506d6f521d5f9294f4
Uncasted <juanferf30@gmail.com>
2022-07-10 22:44:56 -0500

    Add functionality to the generator and its parameters.

--- src/view/password-manager/src/components/Generator/Generator.jsx
+++ src/view/password-manager/src/components/Generator/Generator.jsx
@@ -1,15 +1,18 @@
 // Sketching
 import {images} from "../../App"
 import {useState} from "react"
+import ParameterProvider, {useParameterContext, useParameterContextUpdate} from "./Context/ParameterContext"
 
 export function Generator() {
     return (
         <div className="ml-6 mt-6 flex flex-col text-white">
             <GeneratorHeader/>
-            <PasswordGenerator/>
-            <div className="mt-8 pl-4 pb-4 bg-dark-blue-2 w-[32rem] shadow-md">
-                <GeneratorParameters/>
-            </div>
+            <ParameterProvider>
+                <PasswordGenerator/>
+                <div className="mt-8 pl-4 pb-4 bg-dark-blue-2 w-[32rem] shadow-md">
+                    <GeneratorParameters/>
+                </div>
+            </ParameterProvider>
         </div>
     )
 }
@@ -30,12 +33,25 @@ function GeneratorHeader() {
 }
 
 function PasswordGenerator() {
+    // State
+    const [password, setPassword] = useState("")
+
+    // Context
+    const length = useParameterContext().length
+    const parameters = useParameterContext().parameters
+
+    const generatePassword = () => {
+
+        const generatedPass = window.utility.generateRandomPassword(parameters, length)
+        setPassword(generatedPass)
+    }
+
     return (
         <div className="flex space-x-4">
             <div>
                 <input readOnly
                        type="text"
-                       value="Placeholder"
+                       value={password}
                        onClick={(event) => {
                            // Copy to clipboard.
                            navigator.clipboard.writeText(event.target.value).then()
@@ -43,8 +59,10 @@ function PasswordGenerator() {
                        className="text-black pl-2 w-[32rem] py-1.5 text-lg focus:outline-gray-200"/>
             </div>
             <div>
-                <button className="bg-dark-blue-1 px-8 py-2 shadow-md hover:bg-blue-1 transition active:bg-blue-2
-                focus:outline-gray-200">
+                <button disabled={parameters.length === 0}
+                        onClick={generatePassword}
+                        className="bg-dark-blue-1 px-8 py-2 shadow-md hover:bg-blue-1 transition active:bg-blue-2
+                        focus:outline-gray-200">
                     Generate
                 </button>
             </div>
@@ -53,7 +71,15 @@ function PasswordGenerator() {
 }
 
 function GeneratorParameters() {
-    const [length, setLength] = useState("4")
+    // State
+    const [useLower, setUseLower] = useState(false)
+    const [useUpper, setUseUpper] = useState(false)
+    const [useNumbers, setUseNumbers] = useState(false)
+    const [useSymbols, setUseSymbols] = useState(false)
+
+    // Context
+    const length = useParameterContext().length
+    const update = useParameterContextUpdate()
 
     return (
         <div>
@@ -64,15 +90,24 @@ function GeneratorParameters() {
                 <input type="range"
                        min="4"
                        max="48"
-                       value={length}
+                       defaultValue={length}
                        onChange={(e) => {
-                           setLength(e.target.value)
+                           update.updateLength(e.target.value)
                        }}
                        className="custom-slider slider-progress w-full cursor-pointer focus:outline-gray-200"/>
             </div>
             <div className="flex flex-col space-y-4">
                 <div className="space-x-2 flex items-center">
                     <input type="checkbox"
+                           checked={useLower}
+                           onClick={() => {
+                               if (!useLower) {
+                                   update.addParameter("LOWERCASE")
+                               } else {
+                                   update.delParameter("LOWERCASE")
+                               }
+                               setUseLower(!useLower)
+                           }}
                            className="checkbox checkbox-sm checkbox-primary rounded-none border-2"/>
                     <span className="no-select">
                         Lowercase
@@ -80,6 +115,15 @@ function GeneratorParameters() {
                 </div>
                 <div className="space-x-2 flex items-center">
                     <input type="checkbox"
+                           checked={useUpper}
+                           onClick={() => {
+                               if (!useUpper) {
+                                   update.addParameter("UPPERCASE")
+                               } else {
+                                   update.delParameter("UPPERCASE")
+                               }
+                               setUseUpper(!useUpper)
+                           }}
                            className="checkbox checkbox-sm checkbox-primary rounded-none border-2"/>
                     <span className="no-select">
                         Uppercase
@@ -87,6 +131,15 @@ function GeneratorParameters() {
                 </div>
                 <div className="space-x-2 flex items-center">
                     <input type="checkbox"
+                           checked={useNumbers}
+                           onClick={() => {
+                               if (!useNumbers) {
+                                   update.addParameter("NUMBERS")
+                               } else {
+                                   update.delParameter("NUMBERS")
+                               }
+                               setUseNumbers(!useNumbers)
+                           }}
                            className="checkbox checkbox-sm checkbox-primary rounded-none border-2"/>
                     <span className="no-select">
                         Numbers
@@ -94,6 +147,15 @@ function GeneratorParameters() {
                 </div>
                 <div className="space-x-2 flex items-center">
                     <input type="checkbox"
+                           checked={useSymbols}
+                           onClick={() => {
+                               if (!useSymbols) {
+                                   update.addParameter("SYMBOLS")
+                               } else {
+                                   update.delParameter("SYMBOLS")
+                               }
+                               setUseSymbols(!useSymbols)
+                           }}
                            className="checkbox checkbox-sm checkbox-primary rounded-none border-2"/>
                     <span className="no-select">
                         Symbols

07fac59566a31786a6a46af2d88e7f455479c215
Uncasted <juanferf30@gmail.com>
2022-07-17 17:00:24 -0500

    Add listener to open file manager in the main process.

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ const {app, BrowserWindow, dialog, ipcMain} = require('electron')
 const path = require('path')
 
 // Main Window.
+let mainWin
 const createMainWindow = async () => {
     // Change the icon path depending on the platform."
     const iconPath = process.platform !== "darwin" ? "./build/icon.png" : "./build/icon.icns"
 
-    const mainWin = new BrowserWindow({
+    mainWin = new BrowserWindow({
         width: 1280,
         height: 720,
         minWidth: 900,
@@ -35,9 +36,39 @@ app.on('window-all-closed', () => {
     if (process.platform !== "darwin") app.quit()
 })
 
+// Get the path to create the backup.
+ipcMain.handle("backup:create", async () => {
+    // Options for creating a backup.
+    const options = {
+        title: "Create A Database Backup",
+        buttonLabel: "Create Backup",
+        defaultPath: "backup.json",
+        filters: [
+            {name: "Database File", extensions: ["json"]}
+        ]
+    }
+    // Getting the path.
+    return await handleFileSave(mainWin, options)
+})
+
+// Get the path to load the backup.
+ipcMain.handle("backup:load", async () => {
+    // Options for loading a backup.
+    const options = {
+        title: "Load A Database Backup",
+        buttonLabel: "Load Backup",
+        properties: ["openFile"],
+        filters: [
+            {name: "Database File (JSON)", extensions: ["json"]}
+        ]
+    }
+    // Getting the path.
+    return await handleFileOpen(mainWin, options)
+})
+
 // File Manager functions.
-async function handleFileOpen(options) {
-    const {canceled, filePaths} = await dialog.showOpenDialog(options)
+async function handleFileOpen(window, options) {
+    const {canceled, filePaths} = await dialog.showOpenDialog(window, options)
     // If the user cancels the operation then just cancel.
     if (canceled) {
         return ""
@@ -47,8 +78,8 @@ async function handleFileOpen(options) {
     }
 }
 
-async function handleFileSave(options) {
-    const {canceled, filePath} = await dialog.showSaveDialog(options)
+async function handleFileSave(window, options) {
+    const {canceled, filePath} = await dialog.showSaveDialog(window, options)
     // If the user cancels the operation then just cancel.
     if (canceled) {
         return ""

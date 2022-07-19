bb6b86e319899c60c6d9837b15fdbf0102d91aac
Uncasted <juanferf30@gmail.com>
2022-07-19 13:48:35 -0500

    Make value required. Update naming for onChange and onKeyDown.

--- src/view/src/components/InputNumberBox.jsx
+++ src/view/src/components/InputNumberBox.jsx
@@ -5,9 +5,9 @@ export default function InputNumberBox(props) {
         <input type="number"
                min={props.min || null}
                max={props.max || null}
-               value={props.value || null}
-               onChange={props.inputOnChange || null}
-               onKeyDown={props.inputOnKeyDown || null}
+               value={props.value}
+               onChange={props.onChange || null}
+               onKeyDown={props.onKeyDown || null}
                className="bg-transparent w-8 text-center text-sm rounded-sm focus:outline-gray-200"
         />
     )
@@ -17,6 +17,6 @@ InputNumberBox.propTypes = {
     min: PropTypes.number,
     max: PropTypes.number,
     value: PropTypes.string,
-    inputOnChange: PropTypes.func,
-    inputOnKeyDown: PropTypes.func
+    onChange: PropTypes.func,
+    onKeyDown: PropTypes.func
 }

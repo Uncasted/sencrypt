f9967244602ef5f451318f13972b5fd54e133401
Juan Ferreira <juanferf30@gmail.com>
2022-07-21 15:14:30 -0500

    Create constants file for the model.

---
+++ src/model/constants.js
@@ -0,0 +1,9 @@
+// Constants
+
+// Alphabet characters.
+export const CHARS = {
+    NUMBERS: "012346789",
+    UPPERCASE: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    LOWERCASE: "abcdefghijklmnopqrstuvwxyz",
+    SYMBOLS: "~`! @#$%^&*()_-+={[}]|\\:;\"'<,>.?/"
+}

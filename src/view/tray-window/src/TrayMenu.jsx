7e15fd85f52d9918eed305b95548d57b4fdbd670
Juan Ferreira <juanferf30@gmail.com>
2022-07-29 23:12:10 -0500

    Update onClick handler name for more consistency.

--- src/view/tray-window/src/TrayMenu.jsx
+++ src/view/tray-window/src/TrayMenu.jsx
@@ -27,7 +27,7 @@ export default function TrayMenu () {
                 title={button.title}
                 icon={button.icon}
                 altIcon={button.altIcon}
-                onClick={button.onClick}
+                onClick={button.handleClick}
               />
             </li>
           )

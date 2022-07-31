ee3f619976ec2bfd13b189404f6ae462594c70d1
Uncasted <juanferf30@gmail.com>
2022-07-30 21:39:35 -0500

    Refactoring of main process.

---
+++ filePath.js
@@ -0,0 +1,29 @@
+// File Manager functions.
+const {dialog} = require('electron')
+
+async function handleFileOpen(window, options) {
+  const {canceled, filePaths} = await dialog.showOpenDialog(window, options)
+  // If the user cancels the operation then just cancel.
+  if (canceled) {
+    return ''
+  } else {
+    // Otherwise return the filepath.
+    return filePaths[0]
+  }
+}
+
+async function handleFileSave(window, options) {
+  const {canceled, filePath} = await dialog.showSaveDialog(window, options)
+  // If the user cancels the operation then just cancel.
+  if (canceled) {
+    return ''
+  } else {
+    // Otherwise return the filepath.
+    return filePath
+  }
+}
+
+module.exports = {
+  handleFileOpen,
+  handleFileSave
+}

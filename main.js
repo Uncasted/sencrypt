dff4569b01dd0da2f4b80c6e373eaec00d58f289
Uncasted <juanferf30@gmail.com>
2022-07-30 21:37:51 -0500

    Add tray context menu for Linux. General refactoring.

--- main.js
+++ main.js
@@ -1,19 +1,77 @@
-const { app, BrowserWindow, dialog, ipcMain, Tray } = require('electron')
-const SettingsController = require('./src/controller/settingsController')
+const {app, BrowserWindow, ipcMain, Tray, Menu} = require('electron')
 const path = require('path')
+const login = require("./login")
+const tray = require("./tray")
+const filePath = require("./filePath")
+const {getReloadTime} = require("./login")
 
 // Check if the file explorer is opened.
 let dialogIsOpen = false
-// Time to wait for re-login.
-const reloadTime = getReloadTime()
 // Main Window.
 let MainWin
 // Tray Menu.
 let TrayMenu
 // Tray menu custom window.
 let TrayWin
+// Context menu for tray (Linux).
+const contextMenu = Menu.buildFromTemplate([
+  {
+    label: "Accounts",
+    click: () => {
+      showSection("Accounts")
+    }
+  },
+  {
+    label: "Generator",
+    click: () => {
+      showSection("Generator")
+    }
+  },
+  {
+    label: "Settings",
+    click: () => {
+      showSection("Settings")
+    }
+  },
+  {
+    label: "Exit",
+    click: quitAll
+  }
+])
+
+// Utility functions.
+
+function quitAll() {
+  // Destroy the windows.
+  MainWin.destroy()
+  TrayWin.destroy()
+  TrayMenu.destroy()
+  // Quit the app.
+  app.quit()
+}
+
+function showMainWindow() {
+  // If the main window is hidden, show it.
+  if (!MainWin.isVisible()) {
+    MainWin.show()
+  }
+  // Focus the main window.
+  MainWin.focus()
+}
+
+function showSection(section) {
+  // Hide the tray menu.
+  TrayWin.hide()
+  // Send the signal to the main window.
+  MainWin.webContents.send('view:section', section)
+  // Show the main window.
+  showMainWindow()
+}
+
+// Windows
+
 // Create the main window.
-const createMainWindow = async () => {
+async function createMainWindow() {
   // Change the icon path depending on the platform.
   const iconPath =
     process.platform !== 'darwin' ? './build/icon.png' : './build/icon.icns'
@@ -44,31 +102,28 @@ const createMainWindow = async () => {
 }
 
 // Create Tray window.
-const createTray = () => {
+function createTray() {
   TrayMenu = new Tray(path.join(__dirname, './build/icon.png'))
-
-  // Toggle the tray window.
+  // Tooltip
+  TrayMenu.setToolTip("Sencrypt")
+  // Set the context menu on linux.
+  if (process.platform === 'linux') {
+    TrayMenu.setContextMenu(contextMenu)
+  }
+  // Toggle the tray window. (Windows)
   TrayMenu.on('right-click', () => {
     // If the window is visible hide it, otherwise show it.
-    if (TrayWin.isVisible()) {
-      TrayWin.hide()
-    } else {
-      showWindow()
-    }
+    tray.toggleTrayWindow(TrayWin, TrayMenu)
   })
-  // Focus the main window on double click.
+  // Focus the main window on double click. (Windows)
   TrayMenu.on('double-click', () => {
     // If the main window is hidden, show it.
-    if (!MainWin.isVisible()) {
-      MainWin.show()
-    }
-    // Focus on the window.
-    MainWin.focus()
+    showMainWindow()
   })
 }
 
 // Creates window & specifies its values
-const createTrayWindow = async () => {
+async function createTrayWindow() {
   TrayWin = new BrowserWindow({
     width: 180,
     height: 240,
@@ -88,6 +143,68 @@ const createTrayWindow = async () => {
     TrayWin.hide()
   })
 }
+
+// IPC Main event listeners
+
+// Get the path to create the backup.
+ipcMain.handle('backup:create', async () => {
+  dialogIsOpen = true
+  // Options for creating a backup.
+  const options = {
+    title: 'Create A Database Backup:',
+    buttonLabel: 'Create Backup',
+    defaultPath: 'databaseBackup.json',
+    filters: [{name: 'Database File (JSON)', extensions: ['json']}]
+  }
+  // Getting the path.
+  const path = await filePath.handleFileSave(MainWin, options)
+  dialogIsOpen = false
+  return path
+})
+
+// Get the path to load the backup.
+ipcMain.handle('backup:load', async () => {
+  dialogIsOpen = true
+  // Options for loading a backup.
+  const options = {
+    title: 'Load A Database Backup:',
+    buttonLabel: 'Load Backup',
+    properties: ['openFile'],
+    filters: [{name: 'Database File (JSON)', extensions: ['json']}]
+  }
+  // Getting the path.
+  const path = await filePath.handleFileOpen(MainWin, options)
+  dialogIsOpen = false
+  return path
+})
+
+// Minimizing the window.
+ipcMain.on('mainWin:minimize', () => {
+  MainWin.minimize()
+})
+// Maximize the window.
+ipcMain.on('mainWin:maximize', () => {
+  // If the window is maximized, unmaximize it.
+  if (MainWin.isMaximized()) {
+    MainWin.unmaximize()
+  } else {
+    MainWin.maximize()
+  }
+})
+// Close the window.
+ipcMain.on('mainWin:close', () => {
+  MainWin.hide()
+})
+
+// Open section from the tray menu.
+ipcMain.on('open:section', (event, section) => {
+  showSection(section)
+})
+
+// Close the application from the tray menu.
+ipcMain.on('app:quit', quitAll)
+
+// App event listeners.
 
 // Starting the app.
 app.on('ready', () => {
@@ -113,156 +230,11 @@ app.on('ready', () => {
 
 // Timeout for re-login.
 app.on('browser-window-blur', () => {
-  // If the main window is not focused and reload time is bigger than 0.
-  if (!MainWin.isFocused() && reloadTime && !dialogIsOpen) {
-    // Start the timeout.
-    checkForReload().then()
-  }
-})
-
-// Get the path to create the backup.
-ipcMain.handle('backup:create', async () => {
-  dialogIsOpen = true
-  // Options for creating a backup.
-  const options = {
-    title: 'Create A Database Backup:',
-    buttonLabel: 'Create Backup',
-    defaultPath: 'databaseBackup.json',
-    filters: [{ name: 'Database File (JSON)', extensions: ['json'] }]
-  }
-  // Getting the path.
-  return await handleFileSave(MainWin, options)
-})
-
-// Get the path to load the backup.
-ipcMain.handle('backup:load', async () => {
-  dialogIsOpen = true
-  // Options for loading a backup.
-  const options = {
-    title: 'Load A Database Backup:',
-    buttonLabel: 'Load Backup',
-    properties: ['openFile'],
-    filters: [{ name: 'Database File (JSON)', extensions: ['json'] }]
-  }
-  // Getting the path.
-  return await handleFileOpen(MainWin, options)
-})
-
-// Open section from the tray menu.
-ipcMain.on('open:section', (event, section) => {
-  // Hide the tray menu.
-  TrayWin.hide()
-  // Send the signal to the main window.
-  MainWin.webContents.send('view:section', section)
-  // If the main window is hidden, show it.
-  if (!MainWin.isVisible()) {
-    MainWin.show()
-  }
-  // Focus the main window.
-  MainWin.focus()
-})
-
-// Close the application from the tray menu.
-ipcMain.on('app:quit', () => {
-  // Destroy the windows.
-  MainWin.destroy()
-  TrayWin.destroy()
-  TrayMenu.destroy()
-  // Quit the app.
-  app.quit()
-})
-
-// Minimizing the window.
-ipcMain.on('mainWin:minimize', () => {
-  MainWin.minimize()
-})
-// Maximize the window.
-ipcMain.on('mainWin:maximize', () => {
-  // If the window is maximized, unmaximize it.
-  if (MainWin.isMaximized()) {
-    MainWin.unmaximize()
-  } else {
-    MainWin.maximize()
-  }
-})
-// Close the window.
-ipcMain.on('mainWin:close', () => {
-  MainWin.hide()
-})
-
-// File Manager functions.
-async function handleFileOpen (window, options) {
-  const { canceled, filePaths } = await dialog.showOpenDialog(window, options)
-  dialogIsOpen = false
-  // If the user cancels the operation then just cancel.
-  if (canceled) {
-    return ''
-  } else {
-    // Otherwise return the filepath.
-    return filePaths[0]
-  }
-}
-
-async function handleFileSave (window, options) {
-  const { canceled, filePath } = await dialog.showSaveDialog(window, options)
-  dialogIsOpen = false
-  // If the user cancels the operation then just cancel.
-  if (canceled) {
-    return ''
-  } else {
-    // Otherwise return the filepath.
-    return filePath
-  }
-}
-
-// Function to get reload time.
-async function getReloadTime () {
-  const Controller = new SettingsController()
-  const settings = await Controller.getSettings()
-  // If the login timeout is enabled. get the time.
-  if (settings.loginTimeout) {
-    return settings.loginTimeoutTime
-  } else {
-    return 0
-  }
-}
-
-// Function to check for re-login.
-async function checkForReload () {
-  let currentTime = await reloadTime
-  const checkInterval = setInterval(() => {
-    // Reduce count by one.
-    currentTime--
-    // Clear the interval if the window is focused.
-    if (MainWin.isFocused()) {
-      clearInterval(checkInterval)
+  login.getReloadTime().then(reloadTime => {
+    // If the main window is not focused and reload time is bigger than 0.
+    if (!MainWin.isFocused() && reloadTime && !dialogIsOpen) {
+      // Start the timeout.
+      login.checkForReload(MainWin, reloadTime).then()
     }
-
-    // Reload the window to ask for re-login if the count reaches 0.
-    if (!currentTime) {
-      // Reload the window.
-      MainWin.webContents.reloadIgnoringCache()
-      clearInterval(checkInterval)
-    }
-  }, 1000)
-}
-
-// Function to get the position for the tray window.
-function getTrayPosition () {
-  const windowBounds = TrayWin.getBounds()
-  const trayBounds = TrayMenu.getBounds()
-
-  // Center window horizontally to the tray icon
-  const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
-  // Position the window vertically to the tray icon
-  const y = Math.round(trayBounds.y - windowBounds.height - 4)
-
-  return { x, y }
-}
-
-function showWindow () {
-  const position = getTrayPosition()
-  TrayWin.setPosition(position.x, position.y, false)
-  TrayWin.show()
-  TrayWin.focus()
-}
+  })
+})

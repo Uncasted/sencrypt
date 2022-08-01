d3812298f7c036b8a3c758e2f6a0e19652973f3d
Juan Ferreira <juanferf30@gmail.com>
2022-07-31 23:27:57 -0500

    Add functionality to retract/show sidebar.

--- src/view/application-window/src/components/buttons/ToggleSideBarButton.jsx
+++ src/view/application-window/src/components/buttons/ToggleSideBarButton.jsx
@@ -1,8 +1,17 @@
 import { IMAGES } from '../../data/constants'
+import { useSidebarContextUpdate } from '../../context/SidebarContext'
 
 export default function ToggleSideBarButton () {
+  // Context
+  const { handleCollapse, handleMargin } = useSidebarContextUpdate()
+  // Toggle the margin and the Sidebar.
+  const handleSidebar = () => {
+    handleCollapse()
+    handleMargin()
+  }
+
   return (
-    <button>
+    <button onClick={handleSidebar}>
       <img
         src={IMAGES.SIDEBAR_ICON}
         alt='Sidebar icon'

e1339534ee9d235d5aac0aae40a282d811d62557
Juan Ferreira <100163644+Uncasted@users.noreply.github.com>
2022-07-31 23:47:02 -0500

    Revert "Update the styling of the sidebar."

--- src/view/application-window/src/components/buttons/SidebarButton.jsx
+++ src/view/application-window/src/components/buttons/SidebarButton.jsx
@@ -1,15 +1,15 @@
-import {useEffect, useRef} from 'react'
+import { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 
-export default function SidebarButton(props) {
+export default function SidebarButton (props) {
   const buttonRef = useRef(null)
 
   // Select the default section (Accounts).
   useEffect(() => {
     if (props.defaultSelected) {
       const button = buttonRef.current
-      button.classList.remove('hover:bg-[#00141F]')
-      button.classList.add('bg-[#00293D]', 'hover:bg-[#003D5C]')
+      button.classList.remove('hover:bg-dark-blue-2')
+      button.classList.add('bg-blue-1', 'hover:bg-blue-2')
     }
   }, [])
 
@@ -20,13 +20,13 @@ export default function SidebarButton(props) {
     // Remove selected from any other buttons.
     const menuButtons = document.querySelectorAll('[data-focus="change-focus"]')
     menuButtons.forEach(button => {
-      button.classList.remove('bg-[#00293D]', 'hover:bg-[#003D5C]')
-      button.classList.add('hover:bg-[#00141F]')
+      button.classList.remove('bg-blue-1', 'hover:bg-blue-2')
+      button.classList.add('hover:bg-dark-blue-2')
     })
     // Select the current button.
     const currentButton = buttonRef.current
-    currentButton.classList.remove('hover:bg-[#00141F]')
-    currentButton.classList.add('bg-[#00293D]', 'hover:bg-[#003D5C]')
+    currentButton.classList.remove('hover:bg-dark-blue-2')
+    currentButton.classList.add('bg-blue-1', 'hover:bg-blue-2')
   }
 
   return (
@@ -36,13 +36,13 @@ export default function SidebarButton(props) {
       onClick={() => {
         changeFocus(props.section)
       }}
-      data-focus="change-focus"
-      className="w-full text-start pl-[10px] py-3 hover:bg-[#00141F] transition flex focus:outline-gray-200"
+      data-focus='change-focus'
+      className='w-full text-start pl-[10px] py-3 hover:bg-dark-blue-2 transition flex focus:outline-gray-200'
     >
       <img
         src={props.icon}
-        alt="Accounts section"
-        className="w-7 h-7 mr-4 no-select"
+        alt='Accounts section'
+        className='w-7 h-7 mr-4 no-select'
       />
       {props.title}
     </button>

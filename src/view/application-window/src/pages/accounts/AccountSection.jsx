7bc665a9a2292720ec0d95fd4ad30d57c25bf5da
Juan Ferreira <juanferf30@gmail.com>
2022-07-31 23:30:35 -0500

    Change the section position depending on the sidebar retracted state.

--- src/view/application-window/src/pages/accounts/AccountSection.jsx
+++ src/view/application-window/src/pages/accounts/AccountSection.jsx
@@ -1,9 +1,13 @@
 import AccountsProvider from '../../context/accounts/AccountsContext'
 import { Accounts } from './Accounts'
+import { useSidebarContext } from '../../context/SidebarContext'
 
 export default function AccountSection () {
+  // Context
+  const { margin } = useSidebarContext()
+
   return (
-    <div className='ml-[250px]'>
+    <div className={`${margin} transition-all duration-[250ms] ease-[cubic-bezier(0.25,0.1,0.25,1)] z-0`}>
       <AccountsProvider>
         <Accounts />
       </AccountsProvider>

4cc8c052f4d7fb908eee2d6a24d2ba92e7957418
Juan Ferreira <juanferf30@gmail.com>
2022-07-31 23:26:20 -0500

    Create context for the retractable sidebar state.

---
+++ src/view/application-window/src/context/SidebarContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useContext } from 'react'
+
+const SidebarContext = createContext()
+const SidebarContextUpdate = createContext()
+
+export function useSidebarContext () {
+  return useContext(SidebarContext)
+}
+
+export function useSidebarContextUpdate () {
+  return useContext(SidebarContextUpdate)
+}
+
+export default function SidebarProvider (props) {
+  const [isCollapsed, setIsCollapsed] = useState('show-sidebar')
+  const [margin, setMargin] = useState('ml-[200px]')
+
+  const handleCollapse = () => {
+    setIsCollapsed(prevCollapsed => {
+      return prevCollapsed === 'collapse-sidebar' ? 'show-sidebar' : 'collapse-sidebar'
+    })
+  }
+
+  const handleMargin = () => {
+    setMargin(prevMargin => {
+      return prevMargin === 'ml-[200px]' ? 'ml-[50px]' : 'ml-[200px]'
+    })
+  }
+
+  return (
+    <SidebarContext.Provider value={{ isCollapsed, margin }}>
+      <SidebarContextUpdate.Provider value={{ handleCollapse, handleMargin }}>
+        {props.children}
+      </SidebarContextUpdate.Provider>
+    </SidebarContext.Provider>
+  )
+}

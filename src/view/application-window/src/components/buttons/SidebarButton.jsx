c4e079fc7abce26a72824bf09c5995fb2b94919c
Juan Ferreira <juanferf30@gmail.com>
2022-07-31 23:26:59 -0500

    Update padding to make the icons visible when the sidebar is retracted.

--- src/view/application-window/src/components/buttons/SidebarButton.jsx
+++ src/view/application-window/src/components/buttons/SidebarButton.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 
-export default function NavbarButton (props) {
+export default function SidebarButton (props) {
   const buttonRef = useRef(null)
 
   // Select the default section (Accounts).
@@ -49,7 +49,7 @@ export default function NavbarButton (props) {
   )
 }
 
-NavbarButton.propTypes = {
+SidebarButton.propTypes = {
   changeSelected: PropTypes.func,
   defaultSelected: PropTypes.bool,
   title: PropTypes.string,

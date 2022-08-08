d50e8a62c0af631fa18ad2ee3fbb6e0b7c86290d
Juan Ferreira <juanferf30@gmail.com>
2022-08-07 20:44:25 -0500

    Remove border for the options.

--- src/view/application-window/src/components/Option.jsx
+++ src/view/application-window/src/components/Option.jsx
@@ -2,13 +2,11 @@ import PropTypes from 'prop-types'
 
 export default function Option (props) {
   return (
-    <div className='border-t-2 border-blue-3 mx-20 text-white'>
-      <div className='flex justify-between items-center'>
-        <div className='py-10'>
-          <h1 className='text-md'>{props.label}</h1>
-        </div>
-        <div>{props.children}</div>
+    <div className='flex justify-between items-center'>
+      <div className='py-8'>
+        <h1 className='text-md'>{props.label}</h1>
       </div>
+      <div>{props.children}</div>
     </div>
   )
 }

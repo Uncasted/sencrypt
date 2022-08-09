c69b396ba16741b1c40d7d593d53ae13398ec20f
Juan Ferreira <juanferf30@gmail.com>
2022-08-08 22:41:29 -0500

    Fix the header for the settings section.

--- src/view/application-window/src/components/headers/SectionHeader.jsx
+++ src/view/application-window/src/components/headers/SectionHeader.jsx
@@ -2,17 +2,15 @@ import PropTypes from 'prop-types'
 
 export default function SectionHeader (props) {
   return (
-    <div className='pl-4 pt-6 bg-[#00141f] text-white'>
-      <div className='mb-8 no-select'>
+    <div className='bg-[#00141f] text-white fixed w-full py-8 pl-4 shadow-md z-10'>
+      <div className='no-select flex items-center'>
         <h1
           tabIndex='-1'
           className='text-3xl font-semibold pb-2 flex items-center'
         >
-          <img src={props.icon} alt='Generator icon' className='mr-4' />
+          <img src={props.icon} alt='icon' className='mr-4' />
           {props.children}
         </h1>
-        {/* Header border */}
-        <div className='shadow-md ml-[-1.5rem] h-4' />
       </div>
     </div>
   )

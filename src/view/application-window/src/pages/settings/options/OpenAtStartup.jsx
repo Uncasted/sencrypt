05cadc55d33f117922aaaa370dd3918d39782505
Uncasted <juanferf30@gmail.com>
2022-08-09 17:54:21 -0500

    Create option to open at startup.

---
+++ src/view/application-window/src/pages/settings/options/OpenAtStartup.jsx
@@ -0,0 +1,39 @@
+import Checkbox from '../../../components/Checkbox'
+import Option from '../../../components/Option'
+import { useSettingsContext, useSettingsContextUpdate } from '../../../context/settings/SettingsContext'
+import { useState } from 'react'
+
+export default function OpenAtStartup () {
+  // Context
+  const { openAtStartup } = useSettingsContext()
+  const updateSetting = useSettingsContextUpdate()
+
+  // State
+  const [toggleStartup, setIsStartup] = useState(openAtStartup ?? false)
+
+  const handleStartup = () => {
+    // Update the startup value in the main process.
+    window.settings.toggleStartup(!openAtStartup)
+    // Update the setting.
+    updateSetting('openAtStartup', !openAtStartup)
+    // Update the local state.
+    setIsStartup(openAtStartup => !openAtStartup)
+  }
+
+  return (
+    <Option label='Open at startup'>
+      <Checkbox
+        id='openStartup-option'
+        checked={toggleStartup}
+        onClick={handleStartup}
+        onKeyDown={event => {
+          if (event.key === 'Enter') {
+            // This is bad, but if I don't do this it doesn't work. Properly.
+            const checkbox = document.getElementById('openStartup-option')
+            checkbox.click()
+          }
+        }}
+      />
+    </Option>
+  )
+}

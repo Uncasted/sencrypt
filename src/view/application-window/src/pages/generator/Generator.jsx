d00939bb88424e1f7a9c0dc2dec90d86501dea7b
Juan Ferreira <juanferf30@gmail.com>
2022-08-08 22:38:20 -0500

    Add Generator history to generator section.

--- src/view/application-window/src/pages/generator/Generator.jsx
+++ src/view/application-window/src/pages/generator/Generator.jsx
@@ -3,26 +3,33 @@ import PasswordProvider from '../../context/generator/PasswordContext'
 import PasswordGenerator from './PasswordGenerator'
 import GeneratorParameters from './GeneratorParameters'
 import SectionHeader from '../../components/headers/SectionHeader'
+import GeneratorHistory from './GeneratorHistory'
 import { IMAGES } from '../../data/constants'
+import PasswordListProvider from '../../context/generator/PasswordListContext'
 
 export function Generator () {
   return (
     <>
       <div>
-        <SectionHeader icon={IMAGES.GENERATOR_ICON}>Generator</SectionHeader>
+        <SectionHeader icon={IMAGES.GENERATOR_ICON}>
+          Generator
+        </SectionHeader>
       </div>
-      <div className='ml-6 mt-6 flex flex-col text-white justify-center h-[60vh]'>
+      <div className='ml-6 pt-32 flex flex-col text-white justify-center h-full'>
         <div className='mt-6 mx-auto'>
-          <ParameterProvider>
-            <PasswordProvider>
-              <PasswordGenerator />
-              <div
-                className='relative mt-4 w-[36rem] xl:w-[48rem] shadow-md'
-              >
-                <GeneratorParameters />
-              </div>
-            </PasswordProvider>
-          </ParameterProvider>
+          <PasswordListProvider>
+            <ParameterProvider>
+              <PasswordProvider>
+                <PasswordGenerator />
+                <div
+                  className='relative mt-4 w-[36rem] xl:w-[48rem] shadow-md'
+                >
+                  <GeneratorParameters />
+                  <GeneratorHistory />
+                </div>
+              </PasswordProvider>
+            </ParameterProvider>
+          </PasswordListProvider>
         </div>
       </div>
     </>

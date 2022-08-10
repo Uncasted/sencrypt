ed2dff1a170f79668b96b219bb2c82f5f1fcc410
Uncasted <juanferf30@gmail.com>
2022-08-10 17:08:42 -0500

    Add notification for creating a backup.

--- src/view/application-window/src/pages/settings/options/CreateBackup.jsx
+++ src/view/application-window/src/pages/settings/options/CreateBackup.jsx
@@ -1,10 +1,24 @@
 import PrimaryButton from '../../../components/buttons/PrimaryButton'
 import Option from '../../../components/Option'
+import { useNotificationContextUpdate } from '../../../context/NotificationContext'
+import { IMAGES } from '../../../data/constants'
 
 export default function CreateBackup () {
+  // Context
+  const { handleTitle, handleIcon, handleShow } = useNotificationContextUpdate()
+
   const createBackup = async () => {
     // Create a backup of the database.
-    await window.database.createBackup()
+    const isCreated = await window.database.createBackup()
+    // Show the notification
+    if (isCreated) {
+      handleTitle('Backup Created')
+      handleIcon(IMAGES.SUCCESS_ICON)
+    } else {
+      handleTitle('Invalid Backup Path')
+      handleIcon(IMAGES.CLOSE_ICON)
+    }
+    handleShow()
   }
 
   return (

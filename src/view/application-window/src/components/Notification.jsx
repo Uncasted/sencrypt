4b894646bb2f5161ff5e02a22f200b94e269dfe3
Uncasted <juanferf30@gmail.com>
2022-08-10 17:45:41 -0500

    Change background color to increase contrast.

--- src/view/application-window/src/components/Notification.jsx
+++ src/view/application-window/src/components/Notification.jsx
@@ -4,7 +4,7 @@ export default function Notification (props) {
   return (
     <div
       id='app-notification'
-      className='flex fixed items-center justify-center bg-[#00293d] w-fit px-4 h-[40px] max-h-0 rounded-[25px]
+      className='flex fixed items-center justify-center bg-[#003d5c] w-fit px-4 h-[40px] max-h-0 rounded-[25px]
          bottom-[-20rem] right-[50%] left-[50%] overflow-hidden shadow-sm'
     >
       {props.icon

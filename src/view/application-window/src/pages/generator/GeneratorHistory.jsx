176ff0c8e0a6fad8566cd6f2e36805aadc076037
Uncasted <juanferf30@gmail.com>
2022-08-10 17:45:19 -0500

    Add Clear history button. Remove unnecessary console logs. Reduce border width.

--- src/view/application-window/src/pages/generator/GeneratorHistory.jsx
+++ src/view/application-window/src/pages/generator/GeneratorHistory.jsx
@@ -1,22 +1,43 @@
 import GeneratedPassword from '../../components/GeneratedPassword'
-import { usePasswordListContext } from '../../context/generator/PasswordListContext'
+import { usePasswordListContext, usePasswordListContextUpdate } from '../../context/generator/PasswordListContext'
+import { IMAGES } from '../../data/constants'
+import { useNotificationContextUpdate } from '../../context/NotificationContext'
 
 export default function GeneratorHistory () {
   // Context
   const passwords = usePasswordListContext()
-  console.log(typeof passwords)
-  console.log(passwords)
+  const { clearHistory } = usePasswordListContextUpdate()
+  const { handleTitle, handleIcon, handleShow } = useNotificationContextUpdate()
+
+  const handleClear = () => {
+    // Clear the history.
+    clearHistory()
+    // Show a notification to the user.
+    handleTitle('Cleared History')
+    handleIcon(IMAGES.SUCCESS_ICON)
+    handleShow()
+  }
 
   return (
     <div
-      className='bg-[#00293d] flex flex-col items-center text-white w-full h-[300px] rounded-sm my-12 px-4'
+      className='bg-[#00293d] flex flex-col items-center text-white w-full h-[300px] rounded-sm my-12 px-4
+      relative'
     >
+      <div className='absolute right-4 top-4'>
+        <button onClick={handleClear}>
+          <img
+            src={IMAGES.CLEAR_ICON}
+            alt='Clear History'
+            className='w-7 h-7'
+          />
+        </button>
+      </div>
       <div>
         <h1 className='mt-4 text-lg'>
           Generator History
         </h1>
       </div>
-      <div className='w-full mt-6 border-t-4 border-[#003a57] rounded-sm overflow-scroll'>
+      <div className='w-full mt-6 border-t-2 border-[#003a57] rounded-sm overflow-scroll'>
         {passwords.map((password, index) =>
           <GeneratedPassword
             key={index}

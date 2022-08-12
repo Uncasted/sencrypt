174c642ec8af001c24a51dd023587a159764533d
Uncasted <juanferf30@gmail.com>
2022-08-11 22:19:45 -0500

    Add support for state and handling selection.

--- src/view/application-window/src/components/dropdown/Dropdown.jsx
+++ src/view/application-window/src/components/dropdown/Dropdown.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import { IMAGES } from '../../data/constants'
 import DropdownButton from './DropdownButton'
+import PropTypes from 'prop-types'
 
 export default function Dropdown (props) {
   const [open, setOpen] = useState('dropdown')
 
   const handleOpen = () => {
-    console.log('This function gets called.')
     const dropdown = document.getElementById('dropdown-icon')
     dropdown.animate([{
       transform: open === 'dropdown' ? 'rotate(180deg)' : 'rotate(0deg)'
@@ -37,7 +37,7 @@ export default function Dropdown (props) {
       className='w-[125px] h-[25px] bg-[#00293d] rounded-sm flex justify-between items-center relative text-white
     shadow-sm text-sm mr-4 cursor-pointer'
     >
-      <h1 className='ml-2 no-select'>Minimized</h1>
+      <h1 className='ml-2 no-select'>{props.selected}</h1>
       <img
         id='dropdown-icon'
         src={IMAGES.OPEN_DROPDOWN}
@@ -48,11 +48,14 @@ export default function Dropdown (props) {
         className={`${open} absolute w-full py-1 px-2 space-y-1 bg-[#00293d] rounded-sm mt-24 flex flex-col 
         items-center pointer-events-none`}
       >
-        {props.list.map(option => (
+        {props.options.map(option => (
           <DropdownButton
             key={option.key}
+            index={option.key}
+            id={option.id}
             bgColor={option.bgColor}
             hoverColor={option.hoverColor}
+            handleSelected={props.handleSelected}
           >
             {option.title}
           </DropdownButton>
@@ -60,4 +63,10 @@ export default function Dropdown (props) {
       </div>
     </div>
   )
+}
+
+Dropdown.propTypes = {
+  options: PropTypes.array,
+  selected: PropTypes.string,
+  handleSelected: PropTypes.func
 }

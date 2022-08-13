c5fedfbb44b70464a1a0c6e92e753fb653587de7
Uncasted <juanferf30@gmail.com>
2022-08-12 23:03:19 -0500

    Refactoring.

--- src/view/application-window/src/components/dropdown/DropdownButton.jsx
+++ src/view/application-window/src/components/dropdown/DropdownButton.jsx
@@ -10,7 +10,7 @@ export default function DropdownButton (props) {
       onClick={(event) => {
         // Prevents the button from toggling the dropdown again when clicked.
         event.stopPropagation()
-        props.handleSelected(props.index, props.children)
+        props.onHandleSelected(props.index, props.children)
       }}
       className={`${bgColor} ${hoverColor} w-full transition rounded-md px-2 py-1 text-white pointer-events-auto`}
     >
@@ -24,5 +24,6 @@ DropdownButton.propTypes = {
   bgColor: PropTypes.string,
   hoverColor: PropTypes.string,
   id: PropTypes.string,
-  number: PropTypes.number
+  number: PropTypes.number,
+  onHandleSelected: PropTypes.func
 }

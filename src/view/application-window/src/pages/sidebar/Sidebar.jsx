9eb5e73b89eddbe26988a6d5fc87aeddd110413d
Uncasted <juanferf30@gmail.com>
2022-08-25 21:42:30 -0500

    Add animations to the sidebar.

--- src/view/application-window/src/pages/sidebar/Sidebar.jsx
+++ src/view/application-window/src/pages/sidebar/Sidebar.jsx
@@ -3,10 +3,34 @@ import { IMAGES, SIDEBAR_BUTTONS } from '../../data/constants'
 import SidebarButton from '../../components/buttons/SidebarButton'
 import PropTypes from 'prop-types'
 import { useSidebarContext } from '../../context/SidebarContext'
+import { useAnimationStateContext } from '../../context/AnimationStateContext'
+import { useSpring, animated, easings } from 'react-spring'
 
 export function Sidebar (props) {
   // Context
   const { isCollapsed } = useSidebarContext()
+  const animations = useAnimationStateContext()
+
+  // Animations.
+  const sidebarAnimation = useSpring({
+    from: { opacity: 0, left: '25px', pointerEvents: 'none', userSelect: 'none' },
+    to: { opacity: 1, left: '0', pointerEvents: 'auto', userSelect: 'auto' },
+    cancel: animations.sidebar,
+    config: {
+      duration: 400,
+      easing: easings.easeInOutQuad
+    }
+  })
+
+  const logoutBtnAnimation = useSpring({
+    from: { opacity: 0, left: '25px', pointerEvents: 'none', userSelect: 'none' },
+    to: { opacity: 1, left: '10px', pointerEvents: 'auto', userSelect: 'auto' },
+    cancel: animations.sidebar,
+    config: {
+      duration: 400,
+      easing: easings.easeInOutQuad
+    }
+  })
 
   useEffect(() => {
     const sectionList = document.getElementById('section-list')
@@ -28,7 +52,7 @@ export function Sidebar (props) {
 
   return (
     <nav className={` ${isCollapsed} bg-[#000E14] text-white inset-0 h-[100vh] pt-2 fixed z-10`}>
-      <ul id='section-list' className='flex flex-col text-lg mt-8'>
+      <animated.ul style={sidebarAnimation} id='section-list' className='flex flex-col text-lg mt-8 relative'>
         {SIDEBAR_BUTTONS.map(button => {
           return (
             <li key={button.title}>
@@ -43,8 +67,9 @@ export function Sidebar (props) {
             </li>
           )
         })}
-      </ul>
-      <button
+      </animated.ul>
+      <animated.button
+        style={logoutBtnAnimation}
         onClick={handleLogout}
         className='absolute left-3 bottom-4'
       >
@@ -53,7 +78,7 @@ export function Sidebar (props) {
           alt='log out'
           className='w-7 h-7'
         />
-      </button>
+      </animated.button>
     </nav>
   )
 }

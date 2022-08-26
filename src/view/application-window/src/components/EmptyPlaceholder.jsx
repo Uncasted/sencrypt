dff49a91cb426ac956033be24c852ef39b6ed413
Uncasted <juanferf30@gmail.com>
2022-08-25 19:38:52 -0500

    Add animations to the empty placeholder.

--- src/view/application-window/src/components/EmptyPlaceholder.jsx
+++ src/view/application-window/src/components/EmptyPlaceholder.jsx
@@ -1,16 +1,42 @@
 import { AddAccountButton } from '../pages/accounts/buttons/AddAccountButton'
+import { useTransition, animated, easings } from 'react-spring'
+import PropTypes from 'prop-types'
 
 // This gets rendered when there aren't any buttons in the app.
-export default function EmptyPlaceholder () {
+export default function EmptyPlaceholder (props) {
+  // Animations.
+  const placeholderAnim = useTransition(props.accsLength, {
+    from: { opacity: 0, top: '25vh', pointerEvents: 'none', userSelect: 'none' },
+    enter: { opacity: 1, top: '20vh', pointerEvents: 'auto', userSelect: 'auto' },
+    leave: { opacity: 0, top: '25vh', pointerEvents: 'none', userSelect: 'none' },
+    immediate: !props.isMounted,
+    config: {
+      duration: 250,
+      easing: easings.easeInOutQuad
+    }
+  })
+
   return (
-    <div className='w-full h-[50vh] flex flex-col items-center justify-center'>
-      <h1 className='text-lg lg:text-2xl text-gray-300'>
-        There isn't any accounts to show.
-      </h1>
-      <h1 className='text-md lg:text-lg text-gray-300 pb-6'>
-        Add a new account by clicking on the "Add new account" button.
-      </h1>
-      <AddAccountButton />
-    </div>
+    <>
+      {placeholderAnim((style, accounts) => !accounts &&
+        <animated.div
+          style={style}
+          className='w-full h-[50vh] flex flex-col items-center justify-center absolute inset-0'
+        >
+          <h1 className='text-lg lg:text-2xl text-gray-300'>
+            There isn't any accounts to show.
+          </h1>
+          <h1 className='text-md lg:text-lg text-gray-300 pb-6'>
+            Add a new account by clicking on the "Add new account" button.
+          </h1>
+          <AddAccountButton />
+        </animated.div>
+      )}
+    </>
   )
+}
+
+EmptyPlaceholder.propTypes = {
+  accsLength: PropTypes.number,
+  isMounted: PropTypes.bool
 }

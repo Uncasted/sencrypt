b434ac6d4826983ed58579ff474ebdcd54fdac3b
Uncasted <juanferf30@gmail.com>
2022-08-25 21:39:52 -0500

    Add animation for login screen.

--- src/view/application-window/src/pages/login/LoginForm.jsx
+++ src/view/application-window/src/pages/login/LoginForm.jsx
@@ -3,6 +3,8 @@ import { BLUE_OUTLINE, IMAGES, RED_OUTLINE } from '../../data/constants'
 import InputField from '../../components/forms/InputField'
 import PropTypes from 'prop-types'
 import PrimaryButton from '../../components/buttons/PrimaryButton'
+import { useTransition, animated, useSpring, easings, Globals } from 'react-spring'
+import { useAnimationStateContext, useAnimationStateContextUpdate } from '../../context/AnimationStateContext'
 
 export default function LoginForm (props) {
   // State
@@ -10,10 +12,57 @@ export default function LoginForm (props) {
   const [masterPassword, setMasterPassword] = useState('')
   const [deleteAfterAttempts, setDeleteAfterAttempts] = useState(false)
   const [deleteAttempts, setDeleteAttempts] = useState(0)
+  const [maxWidth, setMaxWidth] = useState(innerWidth)
 
   // Ref
   const warningRef = useRef(null)
   const masterPassRef = useRef(null)
+  const backgroundRef = useRef(null)
+
+  // Context
+  const animations = useAnimationStateContext()
+  const { toggleAnimState } = useAnimationStateContextUpdate()
+
+  // Animations
+  const formTransition = useTransition(isMP, {
+    from: { opacity: 0, left: '100px' },
+    enter: { opacity: 1, left: '0' },
+    leave: { opacity: 0, left: '-100px' },
+    config: {
+      duration: 400,
+      easing: easings.easeInOutQuad
+    }
+  })
+
+  const backgroundSpring = useSpring({
+    // We need to use the innerWidth so that we can use pixels with any window size.
+    // If we use vw here, the animation won't work.
+    from: { maxWidth: `${(maxWidth)}px`, zIndex: 50 },
+    to: { maxWidth: '200px', zIndex: 0 },
+    cancel: animations.loginForm,
+    delay: 400,
+    onRest: () => {
+      // Toggle the next animation.
+      toggleAnimState('appSection')
+      // Make the previous background hidden.
+      const background = backgroundRef.current
+      background.classList.add('hidden')
+    },
+    config: {
+      duration: 750,
+      easing: easings.easeInOutCubic
+    }
+  })
+
+  useEffect(() => {
+    // We need to update the max width for the animation on resize.
+    // If we don't do this the background will remain the same size when resizing the window.
+    window.addEventListener('resize', updateMaxWidth)
+
+    return () => {
+      window.removeEventListener('resize', updateMaxWidth)
+    }
+  }, [])
 
   useEffect(() => {
     // Load the settings at startup.
@@ -39,9 +88,10 @@ export default function LoginForm (props) {
     )
 
     if (isMasterPassword) {
-      // Start the settings.
       // Log in.
       setIsMP(isMasterPassword)
+      // Fade out the login screen.
+      toggleAnimState('loginForm')
     } else {
       // Invalid master password warning.
       const warning = warningRef.current
@@ -69,58 +119,64 @@ export default function LoginForm (props) {
     masterPass.classList.add(...BLUE_OUTLINE)
   }
 
+  const updateMaxWidth = () => {
+    setMaxWidth(innerWidth)
+  }
+
   return (
     <>
-      {!isMP
-        ? (
-          <div className='bg-[#000e14] w-[100vw] h-[100vh] flex flex-col items-center'>
-            <div className='pt-36'>
-              <img src={IMAGES.LOGO} alt='Sencrypt' className='w-[500px]' />
-            </div>
-            <form
-              onSubmit={verifyMasterPassword}
-              className='flex flex-col space-y-6 items-center mt-24 w-72'
-            >
-              <InputField
-                autoFocus
-                bgColor='[#001824]'
-                tabIndex={1}
-                secondaryTabIndex={3}
-                placeholder='Enter Master Password...'
-                title='Enter your Master Password'
-                type='password'
-                fieldId='masterPassword'
-                ref={masterPassRef}
-                name='masterPassword'
-                minLength={1}
-                maxLength={32}
-                value={masterPassword}
-                handleClick={handleWarning}
-                onChange={input => {
-                  setMasterPassword(input)
-                }}
-                hasToggleVisibility
-              />
-              <p ref={warningRef} className='invisible text-red-500'>
-                Invalid Master Password.
-              </p>
-              <PrimaryButton
-                type='submit'
-                tabIndex={2}
-                disabled={!masterPassword}
-                hoverColor='[#003D5C]'
-                activeColor='[#00293d]'
-                width='full'
-                offsetColor='[#000e14]'
-              >
-                Log in
-              </PrimaryButton>
-            </form>
+      <animated.div
+        ref={backgroundRef}
+        style={backgroundSpring}
+        className='bg-[#000e14] w-[100vw] h-[100vh] absolute z-50'
+      />
+      {formTransition((style, isMP) => !isMP
+        ? <animated.div style={style} className='flex flex-col items-center absolute inset-0 z-50'>
+          <div className='pt-36'>
+            <img src={IMAGES.LOGO} alt='Sencrypt' className='w-[500px]' />
           </div>
-          )
-        : (
-            props.children
-          )}
+          <form
+            onSubmit={verifyMasterPassword}
+            className='flex flex-col space-y-6 items-center mt-24 w-72'
+          >
+            <InputField
+              autoFocus
+              bgColor='[#001824]'
+              tabIndex={1}
+              secondaryTabIndex={3}
+              placeholder='Enter Master Password...'
+              title='Enter your Master Password'
+              type='password'
+              fieldId='masterPassword'
+              ref={masterPassRef}
+              name='masterPassword'
+              minLength={1}
+              maxLength={32}
+              value={masterPassword}
+              handleClick={handleWarning}
+              onChange={input => {
+                setMasterPassword(input)
+              }}
+              hasToggleVisibility
+            />
+            <p ref={warningRef} className='invisible text-red-500'>
+              Invalid Master Password.
+            </p>
+            <PrimaryButton
+              type='submit'
+              tabIndex={2}
+              disabled={!masterPassword}
+              hoverColor='[#003D5C]'
+              activeColor='[#00293d]'
+              width='full'
+              offsetColor='[#000e14]'
+            >
+              Log in
+            </PrimaryButton>
+          </form>
+        </animated.div>
+        : props.children
+      )}
     </>
   )
 }
--- src/view/application-window/src/pages/login/NewUserForm.jsx
+++ src/view/application-window/src/pages/login/NewUserForm.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import {
   BLUE_OUTLINE,
   IMAGES,
@@ -11,19 +11,71 @@ import { useWeakPassContext } from '../../context/WeakPassContext'
 import { WeakPasswordModal } from './WeakPasswordModal'
 import PrimaryButton from '../../components/buttons/PrimaryButton'
 import { playWarningSound } from '../../utils/utility'
+import { useTransition, animated, useSpring, easings } from 'react-spring'
+import { useAnimationStateContext, useAnimationStateContextUpdate } from '../../context/AnimationStateContext'
 
 export default function NewUserForm (props) {
   // State
+  const [maxWidth, setMaxWidth] = useState(innerWidth)
   const [isCreatedMP, setIsCreatedMP] = useState(false)
   const [passwords, setPasswords] = useState({
     pass: '',
     confirmPass: ''
   })
-  // Context
-  const isWeak = useWeakPassContext()
 
   // Ref
+  const backgroundRef = useRef(null)
   const warningRef = useRef(null)
+
+  // Context
+  const isWeak = useWeakPassContext()
+  const animations = useAnimationStateContext()
+  const { toggleAnimState } = useAnimationStateContextUpdate()
+
+  // Animations
+  const formTransition = useTransition(isCreatedMP, {
+    from: { opacity: 0, left: '100px' },
+    enter: { opacity: 1, left: '0' },
+    leave: { opacity: 0, left: '-100px' },
+    config: {
+      duration: 400,
+      easing: easings.easeInOutQuad
+    }
+  })
+
+  const backgroundSpring = useSpring({
+    // We need to use the innerWidth so that we can use pixels with any window size.
+    // If we use vw here, the animation won't work.
+    from: { maxWidth: `${(maxWidth)}px`, zIndex: 50 },
+    to: { maxWidth: '200px', zIndex: 0 },
+    cancel: animations.loginForm,
+    delay: 400,
+    onRest: () => {
+      // Toggle the next animation.
+      toggleAnimState('appSection')
+      // Make the previous background hidden.
+      const background = backgroundRef.current
+      background.classList.add('hidden')
+    },
+    config: {
+      duration: 750,
+      easing: easings.easeInOutCubic
+    }
+  })
+
+  useEffect(() => {
+    // We need to update the max width for the animation on resize.
+    // If we don't do this the background will remain the same size when resizing the window.
+    window.addEventListener('resize', updateMaxWidth)
+
+    return () => {
+      window.removeEventListener('resize', updateMaxWidth)
+    }
+  }, [])
+
+  const updateMaxWidth = () => {
+    setMaxWidth(innerWidth)
+  }
 
   const updatePassword = (field, input) => {
     setPasswords(prevPasswords => {
@@ -71,6 +123,8 @@ export default function NewUserForm (props) {
     // Initialize the settings file.
     // Create the master password.
     setIsCreatedMP(isCreated)
+    // Fade out the login form.
+    toggleAnimState('loginForm')
   }
 
   const handleWarning = () => {
@@ -88,62 +142,66 @@ export default function NewUserForm (props) {
 
   return (
     <>
-      {!isCreatedMP
-        ? (
-          <div className='bg-[#000e14] w-[100vw] h-[100vh] flex flex-col items-center'>
-            <WeakPasswordModal handleButton={createMasterPass} />
-            <div className='pt-36'>
-              <img src={IMAGES.LOGO} alt='Sencrypt' className='w-[500px]' />
-            </div>
-            <form
-              onSubmit={verifyPasswords}
-              className='flex flex-col space-y-6 items-center mt-24 w-72'
-            >
-              {LOGIN_FIELDS.map(field => {
-                return (
-                  <InputField
-                    key={field.fieldId}
-                    bgColor='[#001824]'
-                    tabIndex={field.tabIndex}
-                    secondaryTabIndex={field.secondaryTabIndex}
-                    autoFocus={field.autofocus}
-                    title={field.title}
-                    type={field.type}
-                    fieldId={field.fieldId}
-                    name={field.name}
-                    dataOutline={field.dataOutline}
-                    minLength={field.minLength}
-                    maxLength={field.maxLength}
-                    value={passwords[field.value]}
-                    onChange={input => {
-                      updatePassword(field.value, input)
-                    }}
-                    handleClick={handleWarning}
-                    hasToggleVisibility
-                    hasStrengthBar
-                  />
-                )
-              })}
-              <p ref={warningRef} className='invisible text-red-500 mt-2'>
-                The passwords do not match.
-              </p>
-              <PrimaryButton
-                type='submit'
-                disabled={!passwords.pass || !passwords.confirmPass}
-                hoverColor='[#003D5C]'
-                activeColor='[#00293d]'
-                tabIndex={3}
-                width='full'
-                offsetColor='[#000e14]'
-              >
-                Create Vault
-              </PrimaryButton>
-            </form>
+      <animated.div
+        ref={backgroundRef}
+        style={backgroundSpring}
+        className='bg-[#000e14] w-[100vw] h-[100vh] absolute z-50'
+      />
+      <WeakPasswordModal handleButton={createMasterPass} />
+      {formTransition((style, isCreatedMP) => !isCreatedMP
+        ? <animated.div style={style} className='flex flex-col items-center absolute inset-0 z-50'>
+          <div className='pt-24'>
+            <img src={IMAGES.LOGO} alt='Sencrypt' className='w-[500px]' />
           </div>
-          )
+          <form
+            onSubmit={verifyPasswords}
+            className='flex flex-col space-y-6 items-center mt-24 w-72'
+          >
+            {LOGIN_FIELDS.map(field => {
+              return (
+                <InputField
+                  key={field.fieldId}
+                  bgColor='[#001824]'
+                  tabIndex={field.tabIndex}
+                  secondaryTabIndex={field.secondaryTabIndex}
+                  autoFocus={field.autofocus}
+                  title={field.title}
+                  type={field.type}
+                  fieldId={field.fieldId}
+                  name={field.name}
+                  dataOutline={field.dataOutline}
+                  minLength={field.minLength}
+                  maxLength={field.maxLength}
+                  value={passwords[field.value]}
+                  onChange={input => {
+                    updatePassword(field.value, input)
+                  }}
+                  handleClick={handleWarning}
+                  hasToggleVisibility
+                  hasStrengthBar
+                />
+              )
+            })}
+            <p ref={warningRef} className='invisible text-red-500 mt-2'>
+              The passwords do not match.
+            </p>
+            <PrimaryButton
+              type='submit'
+              disabled={!passwords.pass || !passwords.confirmPass}
+              hoverColor='[#003D5C]'
+              activeColor='[#00293d]'
+              tabIndex={3}
+              width='full'
+              offsetColor='[#000e14]'
+            >
+              Create Vault
+            </PrimaryButton>
+          </form>
+          </animated.div>
         : (
             props.children
-          )}
+          )
+      )}
     </>
   )
 }

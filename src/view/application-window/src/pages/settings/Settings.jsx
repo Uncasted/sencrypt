69bc9b00b178e5cd97a7423fa43bdf850f337a60
Uncasted <juanferf30@gmail.com>
2022-08-25 21:41:49 -0500

    Add fade in animation to the settings section.

--- src/view/application-window/src/pages/settings/Settings.jsx
+++ src/view/application-window/src/pages/settings/Settings.jsx
@@ -9,12 +9,23 @@ import { ResetPasswordModal } from './modal/ResetPasswordModal'
 import MinimizeToTray from './options/MinimizeToTray'
 import OpenAtStartup from './options/OpenAtStartup'
 import ToggleTrayProvider from '../../context/settings/ToggleTrayContext'
+import { easings, useSpring, animated } from 'react-spring'
+import EnableAnimations from './options/EnableAnimations'
 
 export default function Settings () {
   // Only show the "open at start up" option in windows.
   const platform = window.utility.getPlatform()
   const isWindows = platform === 'win32'
 
+  // Animations
+  const sectionAnimation = useSpring({
+    from: { opacity: 0, left: '50px' },
+    to: { opacity: 1, left: '0' },
+    config: {
+      duration: 400,
+      easing: easings.easeInOutQuad
+    }
+  })
   return (
     <>
       <ResetPasswordModal />
@@ -23,10 +34,11 @@ export default function Settings () {
           Settings
         </SectionHeader>
       </div>
-      <div className='pt-36 text-white mx-20 text-white space-y-12 pb-8'>
+      <animated.div style={sectionAnimation} className='pt-36 text-white mx-20 text-white space-y-12 pb-8 relative'>
         <div>
           <h1 className='text-lg mb-2'>General</h1>
           <div className='border-t-2 border-b-2 border-[#002133]'>
+            <EnableAnimations />
             <ToggleTrayProvider>
               <MinimizeToTray />
               {isWindows && <OpenAtStartup />}
@@ -48,7 +60,7 @@ export default function Settings () {
             <ResetMasterPassword />
           </div>
         </div>
-      </div>
+      </animated.div>
     </>
   )
 }

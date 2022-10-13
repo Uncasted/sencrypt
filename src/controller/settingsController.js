435b0a3ea8a1e157caed47f3c49f432a3964559e
Uncasted <juanferf30@gmail.com>
2022-10-13 17:16:39 -0500

    Refactor Settings Controller. Add documentation.

--- src/controller/settingsController.js
+++ src/controller/settingsController.js
@@ -1,38 +1,42 @@
-const Settings = require('../model/settings')
+const settings = require('../model/settings')
 
+/** Static controller that manages the settings.*/
 class SettingsController {
-  constructor () {
-    this.Model = new Settings()
+  /**
+   * Controller wrapper for creating the settings for the first time.
+   * @see {settings.createSettings} for more information.
+   **/
+  static createSettings = async () => {
+    await settings.createSettings()
   }
 
-  async start () {
-    try {
-      // Start the settings.
-      await this.Model.start()
-    } catch (error) {
-      console.log('Error at start (Controller).')
-      console.log(error)
-    }
+  /**
+   * Controller wrapper or obtaining a single setting of the settings file.
+   * @see {settings.getSetting} for more information.
+   * @param {string} option Selected Option.
+   * @returns {Promise<string|number|boolean>} Returns the selected option.
+   **/
+  static getSetting = async (option) => {
+    return await settings.getSetting(option)
   }
 
-  async getSettings () {
-    try {
-      // Get the settings.
-      return await this.Model.getSettings()
-    } catch (error) {
-      console.log('Error at getSettings (Controller).')
-      console.log(error)
-    }
+  /**
+   * Controller wrapper to get all the settings from the settings file.
+   * @see {settings.getAllSettings} for more information.
+   * @returns {Promise<Settings>} Settings object.
+   **/
+  static getAllSettings = async () => {
+    return await settings.getAllSettings()
   }
 
-  async updateSetting (option, value) {
-    try {
-      // Update the settings option.
-      await this.Model.updateSetting(option, value)
-    } catch (error) {
-      console.log('Error at updateSetting (Controller).')
-      console.log(error)
-    }
+  /**
+   * Controller wrapper to update a setting's value on the settings file.
+   * @see {settings.updateSetting} for more information.
+   * @param {string} option Selected Option.
+   * @param {string|number|boolean} value New value.
+   **/
+  static updateSetting = async (option, value) => {
+    await settings.updateSetting(option, value)
   }
 }
 
